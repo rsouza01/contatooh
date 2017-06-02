3e13a2a336576a9d6bfc492b157d169ffd736a7a
Rodrigo Souza <rsouza01@gmail.com>
2017-06-02 19:40:21 +0000

    Config added

--- src/server.js
+++ src/server.js
@@ -6,10 +6,13 @@ var http = require('http');
 var express = require('express');
 var app = require('./config/express')();
 
+var CONFIG = require('./config/ambient');
+
+
 require('./config/passport')();
-require('./config/database.js')('mongodb://' + process.env.IP +'/contatooh');
+require('./config/database.js')('mongodb://' + CONFIG.dbAddress +'/contatooh');
 
 
-http.createServer(app).listen(process.env.PORT, process.env.IP, function(){
-	console.log('Contatooh listening on port ' + app.get('port'));
+http.createServer(app).listen(CONFIG.hostPort, CONFIG.hostAddress, function(){
+	console.log('Contatooh listening on port ' + CONFIG.hostPort);
 });
