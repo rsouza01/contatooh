97508179f675d90bebfdc3116a847d76ed9fac41
Rodrigo Souza <rsouza01@gmail.com>
2017-06-04 00:37:21 -0300

    Adjustment in the config/protractor.js file

--- src/config/protractor.js
+++ src/config/protractor.js
@@ -4,17 +4,17 @@ var config = require('./config')();
 
 exports.config = {
 
+	sauceUser: config.sauceUser,
+	sauceKey: config.sauceKey,
+		capabilities : {
+		'name': config.sauceTestName,
+		'browserName': 'chrome',
+		'tunnel-identifier': config.travisJobNumber,
+		'build': config.travisBuild
+	},
+	specs: ['../test/e2e/**/*Spec.js'],
 
 	onPrepare: function () {
-			sauceUser : config.sauceUser,
-			sauceKey : config.sauceKey,
-			capabilities : {
-			'name': config.sauceTestName,
-			'browserName': 'chrome',
-			'tunnel-identifier': config.travisJobNumber,
-			'build': config.travisBuild
-		},
-		specs: ['../test/e2e/**/*Spec.js'],
 
 		browser.driver.get('http://localhost:3000');
 		browser.driver.findElement(by.id('entrar')).click();
