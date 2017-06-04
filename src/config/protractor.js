b82ed1e8b73693cdd8a77c95c4675254e756317e
Rodrigo Souza <rsouza01@gmail.com>
2017-06-04 00:25:51 -0300

    Final adjustments in the code.

--- src/config/protractor.js
+++ src/config/protractor.js
@@ -4,12 +4,18 @@ var config = require('./config')();
 
 exports.config = {
 
-	specs: ['../test/e2e/**/*Spec.js'],
 
 	onPrepare: function () {
+			sauceUser : config.sauceUser,
+			sauceKey : config.sauceKey,
+			capabilities : {
+			'name': config.sauceTestName,
+			'browserName': 'chrome',
+			'tunnel-identifier': config.travisJobNumber,
+			'build': config.travisBuild
+		},
+		specs: ['../test/e2e/**/*Spec.js'],
 
-		//'onPrepare' => é executado apenas uma vez antes dos testes
-		//Aqui usamos  o 'browser.driver' pois a pagina do GitHub não é feita em AngularJS
 		browser.driver.get('http://localhost:3000');
 		browser.driver.findElement(by.id('entrar')).click();
 		browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
